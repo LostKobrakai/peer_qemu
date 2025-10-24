fb8d9568dfb22f7c70d9af7ab04303bb300cd4d7
Benjamin Milde <benni@kobrakai.de>
2025-10-24 20:16:11 +0200

    Changes

--- test/qemu_test.exs
+++ test/qemu_test.exs
@@ -9,17 +9,59 @@ defmodule QemuTest do
   test "talks to qemu nerves using peer" do
     Extrace.calls({:erlang, :open_port, :_}, 10)
 
+    qemu = :os.find_executable(:"qemu-system-aarch64")
+
     {:ok, peer, _node} =
       :peer.start_link(%{
         connection: 0,
-        wait_boot: 60000,
-        exec:
-          {Path.expand("./start_qemu.sh") |> String.to_charlist(),
-           ~w(-machine virt,accel=hvf   -cpu host   -smp 1   -m 256M   -kernel ~/.nerves/artifacts/nerves_system_qemu_aarch64-portable-0.1.1/images/little_loader.elf   -netdev user,id=eth0   -device virtio-net-device,netdev=eth0,mac=fe:db:ed:de:d0:01   -global virtio-mmio.force-legacy=false   -drive if=none,file=virtual-disk.img,format=raw,id=vdisk   -device virtio-blk-device,drive=vdisk,bus=virtio-mmio-bus.0   -nographic   --   -heart -env HEART_BEAT_TIMEOUT 30)c}
+        wait_boot: 50000,
+        exec: {qemu, []},
+        detached: false,
+        args: ~w(-heart -env HEART_BEAT_TIMEOUT 30)c,
+        post_process_args: fn args ->
+          args =
+            args
+            |> Enum.map(fn
+              [58, 58, 49 | _] -> ~c"10.0.2.2"
+              rest -> rest
+            end)
+            |> Enum.join(" ")
+            |> String.replace(",", ",,")
+
+          [
+            ~c"-machine",
+            ~c"virt,accel=hvf",
+            ~c"-cpu",
+            ~c"host",
+            ~c"-smp",
+            ~c"1",
+            ~c"-m",
+            ~c"256M",
+            ~c"-kernel",
+            ~c"/Users/benni/.nerves/artifacts/nerves_system_qemu_aarch64-portable-0.1.1/images/little_loader.elf",
+            ~c"-netdev",
+            ~c"user,id=eth0",
+            ~c"-device",
+            ~c"virtio-net-device,netdev=eth0,mac=fe:db:ed:de:d0:01",
+            ~c"-global",
+            ~c"virtio-mmio.force-legacy=false",
+            ~c"-drive",
+            ~c"if=none,file=virtual-disk.img,format=raw,id=vdisk",
+            ~c"-device",
+            ~c"virtio-blk-device,drive=vdisk,bus=virtio-mmio-bus.0",
+            ~c"-nographic",
+            ~c"-fw_cfg",
+            ~c"name=opt/erl,string=#{args}"
+          ]
+        end
       })
 
     {:ok, result} = :peer.call(peer, Nerves.Runtime.KV, :get_all, [])
 
     IO.inspect(result)
+  rescue
+    e ->
+      Process.sleep(1000)
+      reraise e, __STACKTRACE__
   end
 end
